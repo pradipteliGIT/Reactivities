77cc2ef873b0403a084fd63d5164f6c532296a53
Pradip Teli <vptelipradip@gmail.com>
2021-12-30 14:25:19 +0530

    End of section 4

--- API/Controllers/BaseController.cs
+++ API/Controllers/BaseController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace API.Controllers
 {
@@ -6,5 +8,8 @@ namespace API.Controllers
     [ApiController]
     public class BaseController : ControllerBase
     {
+        private IMediator _mediator;
+
+        protected IMediator Mediator=>_mediator??=HttpContext.RequestServices.GetService<IMediator>();
     }
 }
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
+            //setting db connection
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            //settign CORS policy
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+
+                });
+            });
+            //setting mediator handlers
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            //setting automapper
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
